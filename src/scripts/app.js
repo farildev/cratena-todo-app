305a63b7eb357971b2891aca543edace6a103d07
Faril Mammadov <memmedovfaril@gmail.com>
2022-12-12 17:51:41 +0400

    v1.0

---
+++ src/scripts/app.js
@@ -0,0 +1,19 @@
+const addBtn = document.querySelector('.addtodo__btn');
+const todoInp = document.querySelector('.addtodo__input');
+const checker = document.querySelector('.input-checker');
+const form = document.querySelector('.form__control');
+
+form.addEventListener('submit',formSubmit);
+addBtn.addEventListener('click', valueCheck);
+todoInp
+function valueCheck(){
+    if(todoInp.value === ''){
+        checker.innerHTML = '<p>Dont empty</p>';
+        return;
+    }
+    else{
+        checker.innerHTML = '';
+    }
+}
+
+
