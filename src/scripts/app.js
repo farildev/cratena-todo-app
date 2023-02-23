32dfdd5db0a0294e8788b9eae49762529f2b9094
farildev <memmedovfaril@gmail.com>
2023-02-23 17:03:29 +0400

    v1.0 (Alert messages added to ToDo list app)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,9 +13,10 @@ function eventListeners(){
 function addTodo(e){
     const newTodo = todoInput.value.trim();
 
-    if(todoInput.value == 0){
+    if(todoInput.value === ''){
         checker.innerHTML = "You cannot leave this field blank!"
         todoInput.classList.add('error');
+        checker.classList.add('error');
         console.log('Failed!');
         setTimeout(function(){
             todoInput.classList.remove('error');
@@ -23,6 +24,13 @@ function addTodo(e){
         },2000)
     }
     else{
+        checker.innerHTML = "Todo added successfully!"
+        todoInput.classList.add('success');
+        checker.classList.add('success');
+        setTimeout(function(){
+            checker.innerHTML = ""
+        todoInput.classList.remove('success');
+        },2000)
         addTodoToUI(newTodo);
     }
     e.preventDefault();
@@ -33,6 +41,8 @@ function addTodoToUI(newTodo){
     const deleteBtn = document.createElement("button");
     const checkBtn = document.createElement("button");
     const btnContainer = document.createElement("div");
+
+    /*Customize dynamic elements */
     btnContainer.className = "todo-config-btn";
     checkBtn.className = "  check-btn";
     checkBtn.innerHTML = "<i class='bx bx-check'></i>";
@@ -41,10 +51,17 @@ function addTodoToUI(newTodo){
     btnContainer.appendChild(checkBtn);
     btnContainer.appendChild(deleteBtn);
     todoBox.className = "todo__box";
+
+    /*Append Child */
     todoBox.appendChild(document.createTextNode(newTodo));
     todoBox.appendChild(btnContainer);
     todoUl.appendChild(todoBox);
     todoInput.value = "";
+}
+
+function showAlert(type,message){
+    const alertBox = document.createElement("div");
+    alertBox
 }
 
 function deleteTodo(){
